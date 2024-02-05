0c79be11a3e83931f4f2b5818f32938424290613
Piotr Farbaniec <piotr.farbaniec7@gmail.com>
2024-02-05 21:42:06 +0100

    Task-2--commit_02

--- src/main/java/org/task2/Task2.java
+++ src/main/java/org/task2/Task2.java
@@ -1,15 +1,22 @@
 package org.task2;
 
+import java.io.File;
 import java.io.IOException;
+import java.util.List;
 
 public class Task2 {
     static String pathName = "data.txt";
+    static File sourceFile = new File(pathName);
     static SourceFile file = new SourceFile();
+
 
     public static void main(String[] args) {
         try {
-            for (int i = 0; i < file.readLines(pathName).size() ; i+=2) {
-                System.out.println(file.readLines(pathName).get(i));
+            if (pathName != null && !pathName.isBlank() && sourceFile.exists()) {
+                List<String> text = file.readLines(pathName);
+                for (int i = 0; i < text.size(); i += 2) {
+                    System.out.println(text.get(i));
+                }
             }
         } catch (IOException e) {
             e.getMessage();
