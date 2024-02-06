beca11ac6388cc9aee52046cf18dbaead27ed251
Piotr Farbaniec <piotr.farbaniec7@gmail.com>
2024-02-06 19:58:16 +0100

    Task-2--commit_03

--- src/main/java/org/task2/SourceFile.java
+++ src/main/java/org/task2/SourceFile.java
@@ -1,19 +1,31 @@
 package org.task2;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.nio.file.NoSuchFileException;
 import java.util.ArrayList;
 import java.util.List;
 
 public class SourceFile {
 
-    public List readLines(String pathName) throws IOException {
+    public List readLines(String pathName) throws NullPointerException, IOException {
+        File file  = new File(pathName);
         List<String> linesList = new ArrayList<>();
         String line;
-        try (BufferedReader reader = new BufferedReader(new FileReader(pathName))) {
-            while ((line = reader.readLine()) != null) {
-                linesList.add(line);
+
+        if (pathName == null ) {
+            throw new NullPointerException("File path name can't be null!");
+        }
+        else if (!file.exists() || pathName.isBlank()) {
+            throw new NoSuchFileException("There is no such file exists/no file name provided: " + pathName);
+        }
+        else if (pathName != null && !pathName.isBlank() && file.exists()) {
+            try (BufferedReader reader = new BufferedReader(new FileReader(pathName))) {
+                while ((line = reader.readLine()) != null) {
+                    linesList.add(line);
+                }
             }
         }
         return linesList;
--- src/main/java/org/task2/Task2.java
+++ src/main/java/org/task2/Task2.java
@@ -1,26 +1,26 @@
 package org.task2;
 
-import java.io.File;
 import java.io.IOException;
 import java.util.List;
 
 public class Task2 {
     static String pathName = "data.txt";
-    static File sourceFile = new File(pathName);
     static SourceFile file = new SourceFile();
 
 
     public static void main(String[] args) {
+        List<String> text;
         try {
-            if (pathName != null && !pathName.isBlank() && sourceFile.exists()) {
-                List<String> text = file.readLines(pathName);
-                for (int i = 0; i < text.size(); i += 2) {
-                    System.out.println(text.get(i));
-                }
+            text = file.readLines(pathName);
+            for (int i = 0; i < text.size() ; i+=2) {
+                System.out.println(text.get(i));
             }
-        } catch (IOException e) {
-            e.getMessage();
-            e.printStackTrace();
+        } catch (NullPointerException e1) {
+            System.err.println(e1.getMessage());
+            e1.printStackTrace();
+        } catch (IOException e2) {
+            System.err.println(e2.getMessage());
         }
     }
 }
+
