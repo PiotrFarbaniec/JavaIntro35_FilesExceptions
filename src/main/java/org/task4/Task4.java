e32aba60bcb221e53ed916b7d20417c33555634d
Piotr Farbaniec <piotr.farbaniec7@gmail.com>
2024-02-06 21:57:52 +0100

    Task-4--commit_02

--- src/main/java/org/task4/Task4.java
+++ src/main/java/org/task4/Task4.java
@@ -4,7 +4,7 @@ public class Task4 {
     static String pathName = "data.txt";
     static TextFormatter4 file = new TextFormatter4();
     public static void main(String[] args) {
-        System.out.println(file.isContainMatchesWord(pathName, "sytuacji"));
 
+        System.out.println(file.isContainMatchesWord(pathName, "uporządkowany"));
     }
 }
--- src/main/java/org/task4/TextFormatter4.java
+++ src/main/java/org/task4/TextFormatter4.java
@@ -1,38 +1,54 @@
 package org.task4;
 
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
+import java.nio.file.NoSuchFileException;
 import java.nio.file.Path;
 
 public class TextFormatter4 {
 
-    private String readTextFromFile(String pathName) {
-        String readText = null;
-        try {
-            readText = Files.readString(Path.of(pathName));
-        } catch (IOException e1) {
-            e1.printStackTrace();
+    public String readTextFromFile(String pathName) throws NullPointerException, NoSuchFileException {
+        String readedText = null;
+        File sourceFile = new File(pathName);
+
+        if (pathName == null) {
+            throw new NullPointerException("File path name can't be null!");
         }
-        return readText;
+        else if (!sourceFile.exists() || pathName.isBlank()) {
+            throw new NoSuchFileException("Wrong file path name or no name has been provided");
+        }
+        try {
+            readedText = Files.readString(Path.of(pathName));
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+            e.printStackTrace();
+        }
+        return readedText;
     }
 
+
     public boolean isContainMatchesWord(String pathName, String find) {
-        String text = readTextFromFile(pathName);
-        char[] charArray = (text.toLowerCase()).toCharArray();
-        char[] toFind = (find.toLowerCase()).toCharArray();
-        int n = charArray.length;
-        int m = toFind.length;
+        try {
+            char[] charArray = (readTextFromFile(pathName).toLowerCase()).toCharArray();
+            char[] toFind = (find.toLowerCase()).toCharArray();
+            int n = charArray.length;
+            int m = toFind.length;
 
-        for (int i = 0; i <= n - m; i++) {
-            boolean found = true;
+            for (int i = 0; i <= n - m; i++) {
+                boolean isFound = true;
 
-            for (int j = 0; j < m; j++) {
-                if (charArray[i + j] != toFind[j]) {
-                    found = false;
-                    break;
+                for (int j = 0; j < m; j++) {
+                    if (charArray[i + j] != toFind[j]) {
+                        isFound = false;
+                    }
                 }
+                if (isFound) return true;
             }
-            if (found) return true;
+        } catch (NullPointerException e1) {
+            System.err.println(e1.getMessage());
+        } catch (NoSuchFileException e2) {
+            System.err.println(e2.getMessage());
         }
         return false;
     }
